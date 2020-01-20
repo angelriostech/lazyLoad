80cea2602d3e6f7cbf82def1525b98f5797e1792
Angel Rios <angel.rios@techandsolve.com>
2020-01-20 15:20:06 -0500

    Se modifica y repara el metodo de numero de viajes

--- src/main/java/co/com/tns/lazy/load/domain/business/Trip.java
+++ src/main/java/co/com/tns/lazy/load/domain/business/Trip.java
@@ -6,42 +6,37 @@ import java.awt.List;
  * Para las operaciones de organizar listas y algoritmo de calcular viajes
  */
 public class Trip {
-	
-	
-	
-	
+
 	public int retornarNumeroDeViajes(int[] lista) {
-		int incrementable =0;
-		int cantidadElemetosDisponibles= lista.length;		
+		int incrementable = 0;
+		int cantidadElemetosDisponibles = lista.length;
 		for (int i = 0; i < lista.length; i++) {
-			if(cantidadElemetosDisponibles==0) {
-				return incrementable;
-			}
-			else {
-				if(lista[i]>=50) {
-					incrementable+=1;
-					cantidadElemetosDisponibles--;
+			if (lista[i] >= 50) {
+				incrementable += 1;
+				cantidadElemetosDisponibles--;
+			} else {
+				if (cantidadElemetosDisponibles <= 1) {
+					return incrementable;
 				}
+
 				else {
-					int aumento=2;
-					int pesoBolsa=0;
-					while(pesoBolsa<50 &&cantidadElemetosDisponibles>0) {
-						pesoBolsa=lista[i]*aumento;
+					int aumento = 2;
+					int pesoBolsa = 0;
+					while (pesoBolsa < 50 && cantidadElemetosDisponibles > 0) {
+						pesoBolsa = lista[i] * aumento;
 						aumento++;
-						cantidadElemetosDisponibles-=2;
-						
+						cantidadElemetosDisponibles--;
+
 					}
+					cantidadElemetosDisponibles--;
 					incrementable++;
 				}
-				
+
 			}
-			
-			
+
 		}
 		return incrementable;
-		
-		
-		
+
 	}
-	
+
 }
