f3a8fd2e54732f5f7fe4494270ba3916708521d6
duvan.giraldo <duvan.giraldo@techandsolve.com>
2020-01-20 09:17:17 -0500

    Spring Inicializr project

---
+++ src/test/java/co/com/tns/lazy/load/ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.tns.lazy.load;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
