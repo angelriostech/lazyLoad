112b21b985254a7e5650fc04d371bee56455dd19
duvan.giraldo <duvan.giraldo@techandsolve.com>
2020-01-24 16:46:09 -0500

    Se actualiza Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,7 +7,26 @@ pipeline {
 	}
 
 	stages {
-			
+		stage ('Unit Tests') {
+			steps {	
+				sh 'gradle test'
+			}
+		}
+/*
+		stage('Code Analysis') {
+			steps {
+				withSonarQubeEnv('SonarQubeLocal') {
+					sh 'gradle --info sonarqube -x test'
+				}
+			}
+		}
+*/
+		stage ('Build') {
+			steps {
+				sh 'gradle build'
+			}
+		}
+	
 		stage ('Deployment') {
 		  		steps {
 					sh 'java -jar build/libs/lazy-load-0.0.1-SNAPSHOT.jar &' 
--- src/test/java/co/com/tns/lazy/load/validate/FileValidateTest.java
+++ src/test/java/co/com/tns/lazy/load/validate/FileValidateTest.java
@@ -4,14 +4,21 @@ package co.com.tns.lazy.load.validate;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FileValidateTest {
 
     @InjectMocks
-    FileValidate fileValidate;
+    private FileValidate fileValidate;
+
+    private File file = new File(getClass().getClassLoader().getResource("lazy_loading_example_input.txt").getFile());
 
     @Test
-    public void mustValidateIsNotEmptyFile() {
-
+    public void mustValidateIsNotNullFile() {
+        boolean resutlNullFile = fileValidate.isNullFile(file);
+        assertEquals(false, resutlNullFile);
     }
 
 }
